cc83707c91bea4d03c6e48e8f8ed51578d3c5ce1
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-01 20:34:17 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,9 +46,10 @@ resource "ibm_pi_network" "my_subnet" {
 }
 
 resource "ibm_pi_instance" "lpar1-aix" {
+  for_each              = lpar1-aix
   pi_memory		= each.value.memory
   pi_processors		= each.value.processors #var.lpar1-aix_processors
-  pi_instance_name	= var.lpar1-aix_instance_name
+  pi_instance_name	= 
   pi_proc_type		= var.lpar1-aix_proc_type
   pi_image_id 		= var.lpar1-aix_image_id
   pi_sys_type		= var.lpar1-aix_sys_type
@@ -61,6 +62,7 @@ resource "ibm_pi_instance" "lpar1-aix" {
 }
 
 resource "ibm_pi_instance" "lpar2-linux" {
+  for_each              = lpar2-linux
   pi_memory		= each.value.memory
   pi_processors		= each.value.processors
   pi_instance_name	= var.lpar2-linux_instance_name
